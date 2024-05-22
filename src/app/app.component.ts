a5c5a5a54b2f40183a41d298ae8f454f4fc6e586
a-rRehman <70515831+a-rRehman@users.noreply.github.com>
2024-05-22 14:09:22 +0500

    Created First component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { NewDeliveryOrderComponent } from './new-delivery-order/new-delivery-order.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ButtonModule,
+    NewDeliveryOrderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/new-delivery-order/new-delivery-order.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-delivery-order',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-delivery-order.component.html',
+  styleUrl: './new-delivery-order.component.css'
+})
+export class NewDeliveryOrderComponent {
+
+}
